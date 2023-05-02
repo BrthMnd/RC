376b92919024d9c2e553b7f25e259b70a0f82b6c
Brandon Quintero <developerbvth@gmail.com>
2023-05-02 17:15:23 -0500

    subiendo

--- src/app.js
+++ src/app.js
@@ -5,6 +5,24 @@ const port = 3030;
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
 app.use('/', route);
 
+app.get('/', (req, res, next) => {
+    try {
+        res.render('index', {titulo : "Lo logramos"})
+    } catch (error) {	
+        console.log("hola")
+    }
+})
+app.use((req, res, next) => {
+    res.status(404).render('404' ,{
+        titulo : "404",
+        texto : "La página no existe"
+    })
+});
 app.listen(port, () => { console.log("localhost:" + port) })
